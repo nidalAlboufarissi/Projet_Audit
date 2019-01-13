520f5f024168932863de57879d2a64b5b8e41824
NidalAlboufarissi <alboufarissi.nidal@gmail.com>
2019-01-13 16:17:47 +0100

    Initial commit

---
+++ Projet_Audit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Projet_Audit.MvcApplication" Language="C#" %>
---
+++ Projet_Audit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projet_Audit.Startup))]
+namespace Projet_Audit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
